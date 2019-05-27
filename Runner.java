bf212bc8b931b1712279f0225acf2bdbf5e2af7f
SharonElia <30668337+SharonElia@users.noreply.github.com>
2019-05-27 11:53:25 +0300

    Add files via upload

---
+++ Runner.java
@@ -0,0 +1,23 @@
+package main.java.sh;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+import java.util.Iterator;
+
+public class Runner{
+
+    public Runner() {
+            }
+    public static void main(String[] args) {
+
+        System.out.println("Hello");
+    }
+}
+
+
+
+
+
+
